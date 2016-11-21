ab7ed6ed75455aef08ea48c68e258e4a07421b6f
zwf2015 <zwf2015@163.com>
2016-11-21 12:31:02 +0800

    a

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string url = "http://baidu.com";
+
+            WebRequest req = WebRequest.Create(url);
+
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+
+            Console.WriteLine(res.StatusDescription);
+
+            Stream data = res.GetResponseStream();
+
+            StreamReader sr = new StreamReader(data);
+
+            string result = sr.ReadToEnd();
+
+            Console.WriteLine(result);
+
+            sr.Close();
+            data.Close();
+            res.Close();
+
+            Console.ReadKey();
+        }
+
+        static void DoRequest(string url)
+        {
+            WebRequest req = null;
+            HttpWebResponse res = null;
+            try
+            {
+                req= WebRequest.Create(url);
+                res = (HttpWebResponse)req.GetResponse();
+                Console.WriteLine(res.StatusDescription);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                res.Close();
+                req.Abort();
+                res = null;
+                req = null;
+            }
+        }
+    }
+}
