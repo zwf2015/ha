65b00b156884b90ae2dda6a342d87878f933aea7
zwf2015 <zwf2015@163.com>
2016-11-21 19:02:32 +0800

    task part

--- WindowsFormsApplication1/Main.Designer.cs
+++ WindowsFormsApplication1/Main.Designer.cs
@@ -28,21 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.rtb_output = new System.Windows.Forms.RichTextBox();
+            this.btn_Go = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Cancel);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Go);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.rtb_output);
+            this.splitContainer1.Size = new System.Drawing.Size(415, 244);
+            this.splitContainer1.SplitterDistance = 138;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // rtb_output
+            // 
+            this.rtb_output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtb_output.Location = new System.Drawing.Point(0, 0);
+            this.rtb_output.Name = "rtb_output";
+            this.rtb_output.Size = new System.Drawing.Size(415, 102);
+            this.rtb_output.TabIndex = 0;
+            this.rtb_output.Text = "";
+            // 
+            // btn_Go
+            // 
+            this.btn_Go.Location = new System.Drawing.Point(289, 38);
+            this.btn_Go.Name = "btn_Go";
+            this.btn_Go.Size = new System.Drawing.Size(76, 33);
+            this.btn_Go.TabIndex = 0;
+            this.btn_Go.Text = "go";
+            this.btn_Go.UseVisualStyleBackColor = true;
+            this.btn_Go.Click += new System.EventHandler(this.btn_Go_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(289, 77);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(76, 32);
+            this.btn_Cancel.TabIndex = 1;
+            this.btn_Cancel.Text = "abort";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            this.openFile.FileOk += new System.ComponentModel.CancelEventHandler(this.openFile_FileOk);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(415, 244);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Main";
             this.Text = "HttpsSpider";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox rtb_output;
+        private System.Windows.Forms.Button btn_Go;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.OpenFileDialog openFile;
     }
 }
 
--- WindowsFormsApplication1/Main.cs
+++ WindowsFormsApplication1/Main.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,21 +14,30 @@ namespace WindowsFormsApplication1
 {
     public partial class Main : Form
     {
+        private const string url = "http://baidu.com";
+
+
+        List<string> output_OK = new List<string>();
+        List<string> output_ERROR = new List<string>();
+
+        List<string> input = new List<string>();
+
         public Main()
         {
             ServicePointManager.DefaultConnectionLimit = 512;
             InitializeComponent();
+
+            // Task: http://www.cnblogs.com/pengstone/archive/2012/12/23/2830238.html
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
-            const string url = "http://baidu.com";
+        }
 
-            List<string> output_OK = new List<string>();
-            List<string> output_ERROR = new List<string>();
-
-            List<string> input = new List<string>();
+        private void Test()
+        {
             for (int i = 0; i < 20; i++)
             {
                 input.Add(url);
@@ -79,6 +89,85 @@ namespace WindowsFormsApplication1
             }
 
             return urls;
+        }
+
+        private void btn_Go_Click(object sender, EventArgs e)
+        {
+            TaskScheduler syncContextTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+            Task<int> task = new Task<int>(() =>
+            {
+                return (int)DoRequest(url);
+            });
+
+            var cts = new CancellationTokenSource();
+
+            task.ContinueWith(
+                t =>
+                    {
+                        this.rtb_output.AppendText(string.Format("Result is: {0}.{1}", task.Result.ToString(), Environment.NewLine));
+                    },
+                cts.Token,
+                TaskContinuationOptions.AttachedToParent,
+                syncContextTaskScheduler);
+
+            task.Start();
+        }
+
+        private void openFile_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFD = new OpenFileDialog();
+            oFD.Title = "Open file";
+            oFD.ShowHelp = true;
+            oFD.Filter = "Plain Text|*.txt|All Files|*.*";
+            oFD.FilterIndex = 1;
+            oFD.RestoreDirectory = false;
+            oFD.Multiselect = false;
+            if (oFD.ShowDialog() == DialogResult.OK)
+            {
+                this.rtb_output.AppendText(string.Format("Reading file: {0}...{1}", oFD.FileName, Environment.NewLine));
+
+                Task task = new Task(() =>
+                {
+                    ReadFile(oFD.FileName);
+                });
+
+                task.Start();
+
+                TaskScheduler syncContextTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+                Task cwt = task.ContinueWith(t =>
+                {
+                    this.rtb_output.AppendText(string.Format("Read {0} url(s) form file.{1}", this.input.Count, Environment.NewLine));
+                }, syncContextTaskScheduler);
+            }
+        }
+
+        private void ReadFile(string path)
+        {
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(path);
+                input = lines
+                    .Where(a =>
+                        !string.IsNullOrWhiteSpace(a)
+                        &&
+                        (a.StartsWith("http", StringComparison.OrdinalIgnoreCase)
+                          || a.StartsWith("https", StringComparison.OrdinalIgnoreCase)
+                          || a.StartsWith("//", StringComparison.OrdinalIgnoreCase)
+                        ))
+                    // .Distinct()
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+                this.rtb_output.AppendText(string.Format("Can not read file {0}: {1}!", path, ex.Message));
+            }
         }
     }
 }
